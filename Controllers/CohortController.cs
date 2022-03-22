1ca584a5d08d02a343082f0a8b82f7e13e3fd689
sophieespejo <sophmeas@gmail.com>
2022-03-22 14:52:19 -0700

    adding using statements for cohortModel

--- Controllers/CohortController.cs
+++ Controllers/CohortController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CodewarsBackend.Models;
+using CodewarsBackend.Services;
 
 namespace CodewarsBackend.Controllers
 {
@@ -10,6 +12,17 @@ namespace CodewarsBackend.Controllers
     [Route("[controller]")]
     public class CohortController : ControllerBase
     {
-        
+        private readonly CohortService _data;
+
+        public CohortController(CohortService dataFromService) {
+         _data = dataFromService;
+        }
+
+        //add a new cohort
+        [HttpPost("AddCohort")]
+        public bool AddCohort(CohortModel newCohortModel)
+        {
+            return _data.AddCohort(newCohortModel);
+        }
     }
 }
---
+++ Services/CohortService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodewarsBackend.Models;
+using CodewarsBackend.Services.Context;
+
+namespace CodewarsBackend.Services
+{
+    public class CohortService
+    {
+        
+    }
+}
