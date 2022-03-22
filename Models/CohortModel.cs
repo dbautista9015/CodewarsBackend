ee78e6a23ef5201a3135c897047a55e9b4e43454
sophieespejo <sophmeas@gmail.com>
2022-03-22 14:21:45 -0700

    added datacontext in services folder

--- Models/CohortModel.cs
+++ Models/CohortModel.cs
@@ -7,6 +7,11 @@ namespace CodewarsBackend.Models
 {
     public class CohortModel
     {
-        
+        public int Id { get; set; }
+        public int LvlDifficulty { get; set; }
+        public string DateCreated { get; set; }
+        public bool IsArchived { get; set; }
+
+         public CohortModel(){}
     }
 }
