11509520b11db67b891a0f6c38a9e126f657d280
alsalmiw <alsalmiw@gmail.com>
2022-03-24 11:19:37 -0700

    updating tables

---
+++ Migrations/20220324170824_updatedSeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodewarsBackend.Migrations
+{
+    public partial class updatedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
